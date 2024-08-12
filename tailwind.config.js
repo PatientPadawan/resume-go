39edd40235e64f9de1229ed83c87ab41a8263cd9
Alex Marshall Braden <1patientpadawan@gmail.com>
2024-08-12 15:28:34 -0700

    reconfig tailwindcss for new file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,28 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["internal/templates/*.templ"],
+  content: [
+    "internal/templates/*.templ",
+    "internal/templates/components/*.templ",
+  ],
   theme: {
     extend: {
       colors: {
         "floral-white": "#FFFAF0",
         "soft-blue": "#A0C4FF",
-        "coral": "#FF7F50",
+        coral: "#FF7F50",
         "dark-navy": "#1A2A40",
         "muted-blue": "#4A6FA5",
         "muted-coral": "#E76F51",
         "vd-blue": "#0F172A",
       },
       backgroundColor: {
-        'light': '#FFFAF0', // floral-white
-        'dark': '#0F172A',  // vd-blue
+        light: "#FFFAF0", // floral-white
+        dark: "#0F172A", // vd-blue
       },
       textColor: {
-        'dim': '#1A2A40', // dark-navy
-        'lite': '#FFFAF0',  // floral-white
+        dim: "#1A2A40", // dark-navy
+        lite: "#FFFAF0", // floral-white
       },
     },
   },
