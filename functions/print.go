85fd8f9635bfc7a5c9fbee61b586b61c325fbc4f
Alex Marshall Braden <1patientpadawan@gmail.com>
2024-08-13 18:55:24 -0700

    netlify deployment

---
+++ functions/print.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Parse the template
+	tmpl, err := template.ParseFiles("static/Print-resume.html")
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "Error rendering printable resume",
+		}, nil
+	}
+
+	// Execute the template and capture the output
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, nil)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "Error executing template",
+		}, nil
+	}
+
+	// Return the rendered HTML
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+		Body: buf.String(),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22
 
 require (
 	github.com/a-h/templ v0.2.747
+	github.com/aws/aws-lambda-go v1.47.0
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/kelseyhightower/envconfig v1.4.0
 )
